e5be6809801ff5642cc093b4425682efe1f62c8c
th3Ace <128788548+th3ace7@users.noreply.github.com>
2024-06-08 22:22:08 +0200

    unlocking levels

--- Assets/Resources/finish.cs
+++ Assets/Resources/finish.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class finish : MonoBehaviour
 {
@@ -12,6 +13,11 @@ public class finish : MonoBehaviour
         {
             Time.timeScale = 0;
             panel.gameObject.SetActive(true);
+            
+            if(SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("progressmanager", 1) && SceneManager.GetActiveScene().buildIndex < 3)
+            {
+                PlayerPrefs.SetInt("progressmanager", SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
     }
 }
