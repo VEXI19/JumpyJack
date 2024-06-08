8ae02a06ab7018895e8884bd1afa04e24ab9917e
th3Ace <128788548+th3ace7@users.noreply.github.com>
2024-06-08 15:33:19 +0200

    basic layout: menu, level selection, pause, game finish

---
+++ Assets/userinterface.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class userinterface : MonoBehaviour
+{
+    public GameObject panel;
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    public void play(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Update()
+    {
+        if (panel != null && Input.GetKey(KeyCode.P))
+        {
+            this.pause();
+            panel.gameObject.SetActive(true);
+        }
+    }
+}
